51eb32c25193e36fcfde30775408d13286c68a93
Anderson <anderson.asm201@gmail.com>
2023-03-17 20:58:15 -0500

    Finalizacion del proyecto licuadora interactiva

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 let accionLicuadora = document.querySelector('#licuadoraBoton');
+let licuadora = document.querySelector('#licuadora');
 let estadoLicuadora = 'apagado';
+let sonidoLicuadora = document.querySelector('#licuadoraSonido');
+let sonidoBotonLicuadora = document.querySelector('#licuadoraBotonSonido')
 
 accionLicuadora.addEventListener('click' , controlarLicuadora)
 
 function controlarLicuadora() {
     if(estadoLicuadora === "apagado"){
         estadoLicuadora = 'encendido';
-        console.log('Estoy encendido');
+        licuadora.classList.add('active');
     }else{
         estadoLicuadora = 'apagado';
-        console.log('Estoy apagado');
+        licuadora.classList.remove('active');
+}
+activacionSonido();
+}
+
+function activacionSonido(){
+    if(sonidoLicuadora.paused){
+        sonidoBotonLicuadora.play();
+        sonidoLicuadora.play();
+    }else{
+        sonidoBotonLicuadora.play();
+        sonidoLicuadora.pause();
+        sonidoLicuadora.currentTime = 0;
     }
-};
+}
